157892ab114f1110a93f3f9ac7e2697c3efd4f15
David Stiennon <dstiennon@ackmanndickenson.com>
2015-11-02 14:23:46 -0600

    Added user resource and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+    @gravatar_url = get_gravatar_url @user
+  end
+
+
+  private
+  def get_gravatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    return gravatar_url
+  end
+
 end
