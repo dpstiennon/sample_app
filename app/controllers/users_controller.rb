eae559ff060b9221910b99d96dd1a7625be1540d
David Stiennon <dstiennon@ackmanndickenson.com>
2015-11-05 16:55:15 -0600

    Added delete user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:destroy, :index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
 
 
@@ -44,6 +45,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User deleted'
+    redirect_to users_url
+
+  end
+
 
   def get_gravatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
@@ -67,6 +75,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_url unless  current_user.admin?
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name: 'Example User', email: 'example@railstutorial.org', password: 'foobar', password_confirmation: 'foobar')
+User.create!(name: 'Example User',
+             email: 'example@railstutorial.org',
+             password: 'foobar',
+             password_confirmation: 'foobar',
+             admin: true)
 
 
 99.times do |n|
